db89a17248540d201f3c4efaec6b3fb3831728f5
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2025-06-15 11:08:15 +0530

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -347,7 +347,7 @@ const createStyles = (colors: any) => StyleSheet.create({
   },
   fab: {
     position: 'absolute',
-    bottom: 10,
+    bottom: 20,
     right: 20,
     width: 56,
     height: 56,
