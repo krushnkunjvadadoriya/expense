4ebddde24c96a4782548ee763a2e302d6aaa0f99
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2025-07-08 13:56:49 +0530

    Updated TransactionItem.tsx

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -2,7 +2,7 @@ const rateLimitError = {
   code: "rate-limited",
   message: "You have hit the rate limit. Please upgrade to keep chatting.",
   providerLimitHit: false,
-  isRetryable: true
+  isRetryable: true,
 };
 
 export default rateLimitError;
