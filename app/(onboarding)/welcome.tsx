c181c257a6e9db278c533236436f576ce929d94d
krushnkunjvadadoriya <krushnkunjvadadoriya@gmail.com>
2025-07-18 12:34:42 +0530

    Updated welcome.tsx

--- app/(onboarding)/welcome.tsx
+++ app/(onboarding)/welcome.tsx
@@ -293,7 +293,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 2,
+    elevation: 200,
     backgroundColor: '#e7f0fd'
   },
   featureContent: {
